8741b62cc5286b2ebb19342740649481e5d04391
Saad Shaikh <73577526+saadshaikh1023@users.noreply.github.com>
2024-10-26 11:46:43 +0530

    added the logo on dashboard area

--- DarkStore next app/components/infobar/index.tsx
+++ DarkStore next app/components/infobar/index.tsx
@@ -1,10 +1,11 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-empty-object-type */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 'use client'
-import React, { useEffect } from 'react'
+
+import React from 'react'
 import { Book, Headphones, Search } from 'lucide-react'
-import Templates from '../icons/cloud_download'
 import { Input } from '@/components/ui/input'
+import Image from 'next/image'
 
 import {
   Tooltip,
@@ -16,37 +17,44 @@ import {
 type Props = {}
 
 const InfoBar = (props: Props) => {
-
   return (
-    <div className="flex flex-row justify-end gap-6 items-center px-4 py-4 w-full dark:bg-black ">
-      <span className="flex items-center gap-2 font-bold">
-       <Search />
-        <Input
-          placeholder="Quick Search"
-          className="border-none bg-transparent"
+    <div className="flex flex-row justify-between items-center px-4 py-4 w-full dark:bg-black">
+      <div className="flex items-center gap-2 font-bold">
+      </div>
+      
+      <div className="absolute left-1/2 transform -translate-x-1/2">
+      <a href="/">
+        <Image
+          src="/logo.jpeg" // Replace with your logo path
+          alt="Company Logo"
+          width={210} // Adjust as needed
+          height={40} // Adjust as needed
         />
-      </span>
-      <TooltipProvider>
-        <Tooltip delayDuration={0}>
-          <TooltipTrigger>
-            <Headphones />
-          </TooltipTrigger>
-          <TooltipContent>
-            <p>Contact Support</p>
-          </TooltipContent>
-        </Tooltip>
-      </TooltipProvider>
-      <TooltipProvider>
-        <Tooltip delayDuration={0}>
-          <TooltipTrigger>
-            <Book />
-          </TooltipTrigger>
-          <TooltipContent>
-            <p>Guide</p>
-          </TooltipContent>
-        </Tooltip>
-      </TooltipProvider>
-      {/* <UserButton /> */}
+        </a>
+      </div>
+      
+      <div className="flex items-center gap-6">
+        <TooltipProvider>
+          <Tooltip delayDuration={0}>
+            <TooltipTrigger>
+              <Headphones />
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Contact Support</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+        <TooltipProvider>
+          <Tooltip delayDuration={0}>
+            <TooltipTrigger>
+              <Book />
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Guide</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      </div>
     </div>
   )
 }
